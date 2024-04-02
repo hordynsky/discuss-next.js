15e53d5b0dae41f0e449b527c53cdfc0a3a85784
hordynsky97 <hordynsky@gmail.com>
2024-04-02 16:26:26 +0300

    discuss: next.js project initial

---
+++ src/actions/index.ts
@@ -0,0 +1,7 @@
+import { signIn } from "./sign-in";
+import { signOut } from "./sign-out";
+import { createComment } from "./create-comment";
+import { createPost } from "./create-post";
+import { createTopic } from "./create-topic";
+
+export { signIn, signOut, createComment, createPost, createTopic };
